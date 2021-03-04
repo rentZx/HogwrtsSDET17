7a6ce74ea5a767db01c94e6f7436c1deb677e963
renzixue <renzixue@58.com>
2021-03-04 21:25:50 +0800

    browserDemo AppDemo

--- test_appium/appium_Demo.py
+++ test_appium/appium_Demo.py
@@ -9,9 +9,24 @@
 '''
 from appium import webdriver
 
-des_caps = {}
-des_caps['platformName'] = 'Android'
-des_caps['platformVersion'] = '10.0'
-des_caps['deviceName'] = '7HX5T19918005212'
-drvier = webdriver.Remote("http://localhost:4723/wd/hub", des_caps)
-drvier.quit()
+
+class TestAppDriver():
+    def setup(self):
+        des_caps = {}
+        des_caps['platformName'] = 'Android'
+        # des_caps['platformVersion'] = '6.0'
+        des_caps['deviceName'] = 'emulator-5554'
+        des_caps['appPackage'] = 'com.tencent.wework'
+        des_caps['appActivity'] = '.launch.LaunchSplashActivity'
+        des_caps['noRest'] = True
+        des_caps['skipServerInstallation'] = True
+        des_caps['unicodeKeyBoard'] = "true"
+        des_caps['unicodeKeyBoard'] = "true"
+        self.drvier = webdriver.Remote("http://127.0.0.1:4723/wd/hub", des_caps)
+        self.drvier.implicitly_wait(5)
+
+    def teardown(self):
+        pass
+
+    def test_earch(self):
+        pass
